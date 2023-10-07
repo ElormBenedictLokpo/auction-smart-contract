95439eaf2f97a15f3a741f1fe3daf44fc702f799
ElormBenedictLokpo <officiallokpoelorm@gmail>
2023-10-07 08:09:51 +0000

    :construction: bidders

--- auction.sol
+++ auction.sol
@@ -1,3 +1,50 @@
 //SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;//SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
+
+contract Auction{
+    address owner;
+    uint startTime;
+    uint endTime;
+    uint initialPrice = 0.061 ether;
+
+    enum AuctionStates {Started, Running, Ended, Cancelled}
+
+    struct Bidder{
+        address ethaddress;
+        uint256 amount;
+    }
+
+    mapping(address => Bidder[]) public bidders;
+
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier isOwner(){
+        require(owner == msg.sender, "Only owner is can access functionality");
+        _;
+    }
+
+    modifier notOwner(){
+          require(owner != msg.sender, "Owner cannot access this functionality");
+        _;
+    }
+
+    function bid() public payable notOwner{
+        require(msg.value >= initialPrice, "Cannot bid an amount less than initial price");
+        Bidder memory newBidder = Bidder({
+            ethaddress: msg.sender,
+            amount: msg.value
+        });
+
+        bidders[address(this)].push(newBidder);
+    }
+
+    function getAllBidders() public view returns(Bidder[] memory){
+        return bidders[address(this)];
+    }
+}
